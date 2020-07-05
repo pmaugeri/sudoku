686cf159ba966a499ddbd6dcdf832b111f9f3039
Pascal Maugeri <pmaugeri@akamai.com>
2020-07-05 09:16:15 +0200

    Documentation

--- main.gs.js
+++ main.gs.js
@@ -1,14 +1,11 @@
 /**
- * Add custom menus to this Spreadsheet User Interface.
+ * @fileoverview Simple and fast Sudoku resolver. Main source file
+ *
+ * Git repository: https://github.com/pmaugeri/sudoku
+ * 
+ * @author P.Maugeri
+ * @version 1.0.0
  */
-function onOpen() {
-    var ui = SpreadsheetApp.getUi();
-    ui.createMenu('Sudoku Resolver')
-    .addSeparator()
-    .addItem('Resolve', 'resolve')
-    .addSeparator()
-    .addToUi();
-}
 
 var values = [
     0x1FF, 0x1FF, 0x1FF, 0x1FF, 0x1FF, 0x1FF, 0x1FF, 0x1FF, 0x1FF,
@@ -20,85 +17,172 @@ var values = [
     0x1FF, 0x1FF, 0x1FF, 0x1FF, 0x1FF, 0x1FF, 0x1FF, 0x1FF, 0x1FF,
     0x1FF, 0x1FF, 0x1FF, 0x1FF, 0x1FF, 0x1FF, 0x1FF, 0x1FF, 0x1FF,
     0x1FF, 0x1FF, 0x1FF, 0x1FF, 0x1FF, 0x1FF, 0x1FF, 0x1FF, 0x1FF
-  ];
+];
 
 
-// Source: Bit Twiddling Hacks - https://graphics.stanford.edu/~seander/bithacks.html
-function bitCount (n) {
-n = n - ((n >> 1) & 0x55555555)
-n = (n & 0x33333333) + ((n >> 2) & 0x33333333)
-return ((n + (n >> 4) & 0xF0F0F0F) * 0x1010101) >> 24
+/**
+ * Called when the gdoc spreadsheet document is open. It creates the application menu.
+ */
+function onOpen() {
+    var ui = SpreadsheetApp.getUi();
+    ui.createMenu('Sudoku Resolver')
+        .addSeparator()
+        .addItem('Resolve', 'resolve')
+        .addSeparator()
+        .addToUi();
 }
 
-// Test if a cell contains a unique digit (1, 2, 3 ... 9) and return this digit if true.
-// Returns 0 if the cell contains more than one digit
+/**
+ * A fast method to count the number of binary '1' in a number.
+ * Source: Bit Twiddling Hacks - https://graphics.stanford.edu/~seander/bithacks.html
+ * 
+ * @param {Number} n the number to analyse
+ * @return {Number} number of binary 1 found in the number
+ */
+function bitCount(n) {
+    n = n - ((n >> 1) & 0x55555555)
+    n = (n & 0x33333333) + ((n >> 2) & 0x33333333)
+    return ((n + (n >> 4) & 0xF0F0F0F) * 0x1010101) >> 24
+}
+
+
+/**
+ * Test if a cell contains a unique digit (1, 2, 3 ... 9) and return this digit if true.
+ * 
+ * @param {Number} cell a cell to analyse
+ * @return {Number} 0 if the cell contains more than one digit, or the cell number if it has a unique value
+ */
 function hasUniqueValue(cell) {
-if (bitCount(cell) > 1) return 0;
-return cell;
+    if (bitCount(cell) > 1) return 0;
+    return cell;
 }
 
 
+/**
+ * Encode a cell with a 9-bits binary representation: each Sudoku digit (1,2,...9) corresponds to a binary position
+ * Examples:
+ * 1 => 000000001 => 1
+ * 2 => 000000010 => 2
+ * 3 => 000000100 => 4
+ * 9 => 100000000 => 256
+ * 123 => 000000111 => 7
+ * 123456789 => 111111111 => 511
+ * 
+ * @param {Number} cell a cell to encode
+ * @return {Number} the encoded value
+ */
 function encodeCell(cell) {
-var result = 0;
-for (var i=1; i<10; i++) {
-    if (cell.toString().indexOf(i) > -1) {
-    result += (2 ** (i-1));
+    var result = 0;
+    for (var i = 1; i < 10; i++) {
+        if (cell.toString().indexOf(i) > -1) {
+            result += (2 ** (i - 1));
+        }
     }
-}
-return result;
+    return result;
 }
 
 
+/**
+ * Decode a cell to get a human readable representation.
+ * The internal representation is based on a binary position for each of the possible Sudoku digit (1,2,..9)
+ * 
+ * Examples:
+ * 1 => 000000001 => 1
+ * 7 => 000000111 => 123
+ * 511 => 111111111 = 123456789
+ * 
+ * @param {Number} cell the encoded cell value
+ * @return {Number} the decoded value
+ */
 function decodeCell(cell) {
-var result = "";
-for (var i=1; i<10; i++) {
-    if (cell & (2 ** (i-1))) {
-    result = result + i.toString();
+    var result = "";
+    for (var i = 1; i < 10; i++) {
+        if (cell & (2 ** (i - 1))) {
+            result = result + i.toString();
+        }
     }
-}
-return result;
+    return result;
 }
 
 
+/**
+ * Check if a cell contains a given Sudoku digit
+ * 
+ * @param {Number} cell the cell to analyse
+ * @param {Number} digit the digit to search
+ * @return {Boolean} true if the digit is contained in the cell value
+ */
 function containsDigit(cell, digit) {
-return ((cell & digit) > 0);
+    return ((cell & digit) > 0);
 }
 
 
+/**
+ * Remove a digit from a cell 
+ * 
+ * @param {Number} cell the cell to analyse
+ * @param {Number} digit the digit to search
+ * @return {Number} the result cell value (w/o the digit if found)
+ */
 function removeDigit(cell, digit) {
-return (cell ^ digit);
+    return (cell ^ digit);
 }
 
 
+/**
+ * Reduce a row with a given value. It scans a row of the 9x9 square and 
+ * removes the searched value from the cells that contains this value if 
+ * the cell contains more than one digit.
+ * 
+ * @param {Number} rowNumber the row number of square
+ * @param {Number} valueSearched the value to use to reduce the row
+ * @return {Number} the number of changes applied to the square
+ */
 function reduceRow(rowNumber, valueSearched) {
     var changeCount = 0;
-
-    for (var c=0; c<9; c++) {
-        var currentValue = values[c+(rowNumber*9)];
+    for (var c = 0; c < 9; c++) {
+        var currentValue = values[c + (rowNumber * 9)];
         if (hasUniqueValue(currentValue) == 0 && containsDigit(currentValue, valueSearched)) {
-        values[c+(rowNumber*9)] = removeDigit(currentValue, valueSearched);
-        changeCount++;
+            values[c + (rowNumber * 9)] = removeDigit(currentValue, valueSearched);
+            changeCount++;
         }
 
     }
-
     return changeCount;
 }
 
 
+/**
+ * Reduce a column with a given value. It scans a column of the 9x9 square 
+ * and removes the searched value from the cells that contains this value if 
+ * the cell contains more than one digit.
+ * 
+ * @param {Number} colNumber the column number of square
+ * @param {Number} valueSearched the value to use to reduce the row
+ * @return {Number} the number of changes applied to the square
+ */
 function reduceColumn(colNumber, valueSearched) {
     var changeCount = 0;
-    for (var r=0; r<9; r++) {
-        var currentValue = values[colNumber + (r*9)];
+    for (var r = 0; r < 9; r++) {
+        var currentValue = values[colNumber + (r * 9)];
         if (hasUniqueValue(currentValue) == 0 && containsDigit(currentValue, valueSearched)) {
-        values[colNumber+(r*9)] = removeDigit(currentValue, valueSearched);
-        changeCount++;
+            values[colNumber + (r * 9)] = removeDigit(currentValue, valueSearched);
+            changeCount++;
         }
     }
     return changeCount;
 }
 
 
+/**
+ * Reduce a column and a row with a given value. It calls reduceColumn() and
+ * reduceRow().
+ * 
+ * @param {Number} colNumber the column number of square
+ * @param {Number} rowNumber the row number of square
+ * @param {Number} valueSearched the value to use to reduce the row/column
+ * @return {Number} the number of changes applied to the square
+ */
 function reduceRowAndColumn(rowNumber, colNumber, valueSearched) {
     var changeCount = 0;
     if (hasUniqueValue(valueSearched) != 0) {
@@ -109,6 +193,12 @@ function reduceRowAndColumn(rowNumber, colNumber, valueSearched) {
 }
 
 
+/**
+ * Scan all square cells and if a unique digit is found it calls 
+ * reduceRowAndColumn() for this digit.
+ * 
+ * @return {Number} the number of changes applied to the square
+ */
 function reduceAllRowsAndColumns() {
 
     var changeCount = 0;
@@ -118,11 +208,11 @@ function reduceAllRowsAndColumns() {
     do {
 
         var itrChange = 0;
-        
-        for (var i=0; i<81; i++) {
-            if (hasUniqueValue(values[i]) !=0) {
-                var rowNumber = Math.floor(i/9);
-                var colNumber = i%9;
+
+        for (var i = 0; i < 81; i++) {
+            if (hasUniqueValue(values[i]) != 0) {
+                var rowNumber = Math.floor(i / 9);
+                var colNumber = i % 9;
                 itrChange += reduceRowAndColumn(rowNumber, colNumber, values[i]);
             }
         }
@@ -135,13 +225,14 @@ function reduceAllRowsAndColumns() {
 }
 
 
-
-
-
-
-
-
-
+/**
+ * The main function that will perform reductions of the rows, columns and 
+ * squares in an attempt to resolve the Sudoku square.
+ * 
+ * It will keep calling in sequence reduceAllRowsAndColumns() and 
+ * reduceAllSquares() while changes have been applied by one of these 
+ * functions.
+ */
 function resolve() {
     var change1 = 0;
     var changeCount = 0;
@@ -150,30 +241,30 @@ function resolve() {
     sheetValues = range.getValues();
 
     // Encode values
-    for (var c=0; c<9; c++) {
-        for (var r=0; r<9; r++) {
+    for (var c = 0; c < 9; c++) {
+        for (var r = 0; r < 9; r++) {
             if (sheetValues[c][r] != 0) {
-                values[c+(r*9)] = encodeCell(sheetValues[c][r]);
+                values[c + (r * 9)] = encodeCell(sheetValues[c][r]);
             }
         }
-    }  
+    }
 
     do {
         change1 = reduceAllRowsAndColumns();
-        change2 = reduceAllSquares();
+        change2 = reduceAllSquares(values);
         changeCount += change1;
         changeCount += change2;
-    } while ((change1 != 0)||(change2 != 0));
+    } while ((change1 != 0) || (change2 != 0));
 
     // Decode values
-    for (var c=0; c<9; c++) {
-      for (var r=0; r<9; r++) {
-        sheetValues[c][r] = decodeCell(values[c+(r*9)]);
-      }
+    for (var c = 0; c < 9; c++) {
+        for (var r = 0; r < 9; r++) {
+            sheetValues[c][r] = decodeCell(values[c + (r * 9)]);
+        }
     }
 
     // Update spreadsheet with the result values  
-    range.setValues(sheetValues);  
+    range.setValues(sheetValues);
 
     var r = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet().getRange("A12");
     r.setValue(changeCount);
--- square.gs.js
+++ square.gs.js
@@ -1,3 +1,24 @@
+/**
+ * @fileoverview Simple and fast Sudoku resolver. 3x3 square related functions.
+ *
+ * Git repository: https://github.com/pmaugeri/sudoku
+ * 
+ * @author P.Maugeri
+ * @version 1.0.0
+ */
+
+
+
+/**
+ * Reduce a 3x3 square in 2 phases.
+ * The first phase detects a cell with a single digit and removes this digits from the other 8 square cells.
+ * The secode phase detects cells with multiple digits and check if one of the digit is found only in this cell. If yes if will reduce the cell with the digit found (only possibility for the digit).
+ * 
+ * @param {String} squareValues the whole 9x9 square values. The changes will appear in this array
+ * @param {String} c0 the column index of the 3x3 square within the 9x9 square
+ * @param {String} r0 the row index of the 3x3 square within the 9x9 square
+ * @return {String} the number of changes applied
+ */
 function reduceSquare(squareValues, c0, r0) {
 
   var changeCount = 0;
@@ -5,32 +26,32 @@ function reduceSquare(squareValues, c0, r0) {
   // Scan for cell with a single digit
   do {
     var oldChangeCount = changeCount;
-    for (var c=c0; c<(c0+3); c++) {
-      for (var r=r0; r<(r0+3); r++) {
-        var valueSearched = hasUniqueValue(squareValues[c+(r*9)]);
-        if (valueSearched > 0) {                
-          for (var c2=c0; c2<(c0+3); c2++) {
-            for (var r2=r0; r2<(r0+3); r2++) {
-              if ( (hasUniqueValue(squareValues[c2+(r2*9)]) == 0) && containsDigit(squareValues[c2+(r2*9)], valueSearched)) {
-                squareValues[c2+(r2*9)] = removeDigit(squareValues[c2+(r2*9)], valueSearched);
-                changeCount++;              
+    for (var c = c0; c < (c0 + 3); c++) {
+      for (var r = r0; r < (r0 + 3); r++) {
+        var valueSearched = hasUniqueValue(squareValues[c + (r * 9)]);
+        if (valueSearched > 0) {
+          for (var c2 = c0; c2 < (c0 + 3); c2++) {
+            for (var r2 = r0; r2 < (r0 + 3); r2++) {
+              if ((hasUniqueValue(squareValues[c2 + (r2 * 9)]) == 0) && containsDigit(squareValues[c2 + (r2 * 9)], valueSearched)) {
+                squareValues[c2 + (r2 * 9)] = removeDigit(squareValues[c2 + (r2 * 9)], valueSearched);
+                changeCount++;
               }
             }
           }
         }
       }
     }
-  } while ( (changeCount - oldChangeCount) > 0);
-    
+  } while ((changeCount - oldChangeCount) > 0);
+
   do {
     var oldChangeCount = changeCount;
-    for (var i=0; i<9; i++) {
+    for (var i = 0; i < 9; i++) {
       var valueSearched = 2 ** i;
       var lastCol = -1;
       var lastRow = -1;
-      for (var c=c0; c<(c0+3); c++) {
-        for (var r=r0; r<(r0+3); r++) {      
-          if ( (hasUniqueValue(squareValues[c+(r*9)]) == 0) && containsDigit(squareValues[c+(r*9)], valueSearched) ) {
+      for (var c = c0; c < (c0 + 3); c++) {
+        for (var r = r0; r < (r0 + 3); r++) {
+          if ((hasUniqueValue(squareValues[c + (r * 9)]) == 0) && containsDigit(squareValues[c + (r * 9)], valueSearched)) {
             if (lastCol == -1) {
               lastCol = c;
               lastRow = r;
@@ -46,23 +67,30 @@ function reduceSquare(squareValues, c0, r0) {
         if (lastCol == -2) break;
       }
       if (lastCol > -1) {
-        squareValues[lastCol+(lastRow*9)] = valueSearched;
-        changeCount++;              
+        squareValues[lastCol + (lastRow * 9)] = valueSearched;
+        changeCount++;
       }
     }
-  } while ( (changeCount - oldChangeCount) > 0);
-  
- return changeCount;
-}
+  } while ((changeCount - oldChangeCount) > 0);
 
-
-function reduceAllSquares() {
-  var changeCount = 0;
-  for (var row=0; row<3; row++) {
-    changeCount += reduceSquare(values, 0, (row*3));
-    changeCount += reduceSquare(values, 3, (row*3));
-    changeCount += reduceSquare(values, 6, (row*3));
-  }
   return changeCount;
 }
 
+
+/**
+ * Scan the nine 3x3 squares and call reduceSquare() for each one.
+ * The first phase detects a cell with a single digit and removes this digits from the other 8 square cells.
+ * The secode phase detects cells with multiple digits and check if one of the digit is found only in this cell. If yes if will reduce the cell with the digit found (only possibility for the digit).
+ * 
+ * @param {String} squareValues the whole 9x9 square values. The changes will appear in this array
+ * @return {String} the number of changes applied
+ */
+function reduceAllSquares(squareValues) {
+  var changeCount = 0;
+  for (var row = 0; row < 3; row++) {
+    changeCount += reduceSquare(squareValues, 0, (row * 3));
+    changeCount += reduceSquare(squareValues, 3, (row * 3));
+    changeCount += reduceSquare(squareValues, 6, (row * 3));
+  }
+  return changeCount;
+}
