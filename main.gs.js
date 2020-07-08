5a487589090889d08bab5d1395ee0cba0b9e0cf2
Pascal Maugeri <pmaugeri@akamai.com>
2020-07-08 23:54:07 +0200

    Add reduction by vectors.

--- main.gs.js
+++ main.gs.js
@@ -4,8 +4,20 @@
  * Git repository: https://github.com/pmaugeri/sudoku
  * 
  * @author P.Maugeri
- * @version 1.0.0
+ * @version 1.1.0
  */
+
+var COLUMNS = "ABCDEFGHI"
+
+var BG_COLORS = ["#ff0000",
+                 "#ff9900",
+                 "#ffff00",
+                 "#00ff00",
+                 "#00ffff", 
+                 "#4a86e8", 
+                 "#0000ff", 
+                 "#9900ff", 
+                 "#ff00ff" ]
 
 var values = [
     0x1FF, 0x1FF, 0x1FF, 0x1FF, 0x1FF, 0x1FF, 0x1FF, 0x1FF, 0x1FF,
@@ -29,6 +41,8 @@ function onOpen() {
         .addSeparator()
         .addItem('Resolve', 'resolve')
         .addSeparator()
+        .addItem('Show small vectors', 'showSmallVectors')
+        .addItem('Show long vectors', 'showLongVectors')
         .addToUi();
 }
 
@@ -136,17 +150,20 @@ function removeDigit(cell, digit) {
  * 
  * @param {Number} rowNumber the row number of square
  * @param {Number} valueSearched the value to use to reduce the row
+ * @param {Number} startExclusionC the column number should be less than startExclusionC
+ * @param {Number} endExclusionC the column number should be greater than endExclusionC
  * @return {Number} the number of changes applied to the square
  */
-function reduceRow(rowNumber, valueSearched) {
+function reduceRow(rowNumber, valueSearched, startExclusionC, endExclusionC) {
     var changeCount = 0;
     for (var c = 0; c < 9; c++) {
+      if ( (c < startExclusionC) || (c > endExclusionC) ) {
         var currentValue = values[c + (rowNumber * 9)];
         if (hasUniqueValue(currentValue) == 0 && containsDigit(currentValue, valueSearched)) {
             values[c + (rowNumber * 9)] = removeDigit(currentValue, valueSearched);
             changeCount++;
         }
-
+      }
     }
     return changeCount;
 }
@@ -159,16 +176,20 @@ function reduceRow(rowNumber, valueSearched) {
  * 
  * @param {Number} colNumber the column number of square
  * @param {Number} valueSearched the value to use to reduce the row
+ * @param {Number} startExclusionR the row number should be less than startExclusionR
+ * @param {Number} endExclusionR the row number should be greater than endExclusionR
  * @return {Number} the number of changes applied to the square
  */
-function reduceColumn(colNumber, valueSearched) {
+function reduceColumn(colNumber, valueSearched, startExclusionR, endExclusionR) {
     var changeCount = 0;
     for (var r = 0; r < 9; r++) {
+      if ( (r < startExclusionR) || (r > endExclusionR) ) {
         var currentValue = values[colNumber + (r * 9)];
         if (hasUniqueValue(currentValue) == 0 && containsDigit(currentValue, valueSearched)) {
             values[colNumber + (r * 9)] = removeDigit(currentValue, valueSearched);
             changeCount++;
         }
+      }
     }
     return changeCount;
 }
@@ -186,8 +207,8 @@ function reduceColumn(colNumber, valueSearched) {
 function reduceRowAndColumn(rowNumber, colNumber, valueSearched) {
     var changeCount = 0;
     if (hasUniqueValue(valueSearched) != 0) {
-        changeCount += reduceRow(rowNumber, valueSearched);
-        changeCount += reduceColumn(colNumber, valueSearched);
+        changeCount += reduceRow(rowNumber, valueSearched, 10, 10);
+        changeCount += reduceColumn(colNumber, valueSearched, 10, 10);
     }
     return changeCount;
 }
@@ -241,10 +262,10 @@ function resolve() {
     sheetValues = range.getValues();
 
     // Encode values
-    for (var c = 0; c < 9; c++) {
-        for (var r = 0; r < 9; r++) {
-            if (sheetValues[c][r] != 0) {
-                values[c + (r * 9)] = encodeCell(sheetValues[c][r]);
+    for (var r = 0; r < 9; r++) {
+      for (var c = 0; c < 9; c++) {
+            if (sheetValues[r][c] != 0) {
+                values[c + (r * 9)] = encodeCell(sheetValues[r][c]);
             }
         }
     }
@@ -252,22 +273,182 @@ function resolve() {
     do {
         change1 = reduceAllRowsAndColumns();
         change2 = reduceAllSquares(values);
-        changeCount += change1;
-        changeCount += change2;
-    } while ((change1 != 0) || (change2 != 0));
-
+        change3 = findVectors();
+        changeCount = changeCount + change1 + change2 + change3;
+    } while ((change1 != 0) || (change2 != 0) || (change3 != 0));
+  
     // Decode values
-    for (var c = 0; c < 9; c++) {
-        for (var r = 0; r < 9; r++) {
-            sheetValues[c][r] = decodeCell(values[c + (r * 9)]);
+    for (var r = 0; r < 9; r++) {
+      for (var c = 0; c < 9; c++) {
+            sheetValues[r][c] = decodeCell(values[c + (r * 9)]);
         }
     }
 
     // Update spreadsheet with the result values  
     range.setValues(sheetValues);
 
-    var r = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet().getRange("A12");
+    var r = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet().getRange("D12");
     r.setValue(changeCount);
 
 }
 
+function findVectorsInSquare(squareValues, c0, r0, small, long) {
+  var vectors = [];
+  var digitCandidates = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  var changeCount = 0;
+
+  for (var d=1; d<10; d++) {
+    for (var c = c0; c < (c0 + 3); c++) {
+      for (var r = r0; r < (r0 + 3); r++) {
+      	var cell = squareValues[c + (r * 9)];
+        if ((hasUniqueValue(cell) == 0) && containsDigit(cell, (2**(d-1))) )
+          digitCandidates[d] = digitCandidates[d] + 1;
+      }
+    }
+  }
+  var smallVector = new Array();
+  var longVector = new Array();
+  for (var i=1; i<10; i++) {
+    if (digitCandidates[i] == 2)
+      smallVector.push(i);
+    if (digitCandidates[i] == 3)
+      longVector.push(i);
+  }
+  
+  
+  // Search for 2-digit vectors
+  if (small) {
+    for (var i=0; i<smallVector.length; i++) {
+      var searchedValue = 2**(smallVector[i]-1);
+      // Scan rows for the vector
+      for (var r = r0; r < (r0 + 3); r++) {
+        if ( (containsDigit(squareValues[c0 + (r * 9)], searchedValue) && containsDigit(squareValues[c0+1 + (r * 9)], searchedValue)) ) {
+          Logger.info('Found a 2-digit vector with digit ' + smallVector[i] + ' on row ' + r);
+          SpreadsheetApp.getActiveSpreadsheet().getActiveSheet().getRange(COLUMNS.charAt(c0).toString()+(r+1)).setBackground(BG_COLORS[smallVector[i]-1]);
+          SpreadsheetApp.getActiveSpreadsheet().getActiveSheet().getRange(COLUMNS.charAt(c0+1).toString()+(r+1)).setBackground(BG_COLORS[smallVector[i]-1]);
+          changeCount += reduceRow(r, searchedValue, c0, (c0+2));
+        }
+        if ( (containsDigit(squareValues[c0+1 + (r * 9)], searchedValue) && containsDigit(squareValues[c0+2 + (r * 9)], searchedValue)) ) {
+          Logger.info('Found a 2-digit vector with digit ' + smallVector[i] + ' on row ' + r);
+          SpreadsheetApp.getActiveSpreadsheet().getActiveSheet().getRange(COLUMNS.charAt(c0+1).toString()+(r+1)).setBackground(BG_COLORS[smallVector[i]-1]);
+          SpreadsheetApp.getActiveSpreadsheet().getActiveSheet().getRange(COLUMNS.charAt(c0+2).toString()+(r+1)).setBackground(BG_COLORS[smallVector[i]-1]);
+          changeCount += reduceRow(r, searchedValue, c0, (c0+2));
+        }
+        if ( (containsDigit(squareValues[c0 + (r * 9)], searchedValue) && containsDigit(squareValues[c0+2 + (r * 9)], searchedValue)) ) {
+          Logger.info('Found a 2-digit vector with digit ' + smallVector[i] + ' on row ' + r);
+          SpreadsheetApp.getActiveSpreadsheet().getActiveSheet().getRange(COLUMNS.charAt(c0).toString()+(r+1)).setBackground(BG_COLORS[smallVector[i]-1]);
+          SpreadsheetApp.getActiveSpreadsheet().getActiveSheet().getRange(COLUMNS.charAt(c0+2).toString()+(r+1)).setBackground(BG_COLORS[smallVector[i]-1]);
+          changeCount += reduceRow(r, searchedValue, c0, (c0+2));
+        }
+      }
+      // Scan columns for the vector
+      for (var c = c0; c < (c0 + 3); c++) {
+        if ( (containsDigit(squareValues[c + (r0 * 9)], searchedValue) && containsDigit(squareValues[c + ((r0+1) * 9)], searchedValue)) ) {
+          Logger.info('Found a 2-digit vector with digit ' + smallVector[i] + ' on column ' + c);
+          SpreadsheetApp.getActiveSpreadsheet().getActiveSheet().getRange(COLUMNS.charAt(c).toString()+(r0+1)).setBackground(BG_COLORS[smallVector[i]-1]);
+          SpreadsheetApp.getActiveSpreadsheet().getActiveSheet().getRange(COLUMNS.charAt(c).toString()+(r0+1+1)).setBackground(BG_COLORS[smallVector[i]-1]);
+          changeCount += reduceColumn(c, searchedValue, r0, (r0+2));
+        }
+        if ( (containsDigit(squareValues[c + (r0 * 9)], searchedValue) && containsDigit(squareValues[c + ((r0+2) * 9)], searchedValue)) ) {
+          Logger.info('Found a 2-digit vector with digit ' + smallVector[i] + ' on column ' + c);
+          SpreadsheetApp.getActiveSpreadsheet().getActiveSheet().getRange(COLUMNS.charAt(c).toString()+(r0+1)).setBackground(BG_COLORS[smallVector[i]-1]);
+          SpreadsheetApp.getActiveSpreadsheet().getActiveSheet().getRange(COLUMNS.charAt(c).toString()+(r0+2+1)).setBackground(BG_COLORS[smallVector[i]-1]);
+          changeCount += reduceColumn(c, searchedValue, r0, (r0+2));
+        }
+        if ( (containsDigit(squareValues[c + ((r0+1) * 9)], searchedValue) && containsDigit(squareValues[c + ((r0+2) * 9)], searchedValue))) {
+          Logger.info('Found a 2-digit vector with digit ' + smallVector[i] + ' on column ' + c);
+          SpreadsheetApp.getActiveSpreadsheet().getActiveSheet().getRange(COLUMNS.charAt(c).toString()+(r0+1+1)).setBackground(BG_COLORS[smallVector[i]-1]);
+          SpreadsheetApp.getActiveSpreadsheet().getActiveSheet().getRange(COLUMNS.charAt(c).toString()+(r0+2+1)).setBackground(BG_COLORS[smallVector[i]-1]);
+          changeCount += reduceColumn(c, searchedValue, r0, (r0+2));
+        }
+      }
+    }
+  }
+
+  // Search for 3-digit vectors
+  if (long) {   
+    for (var i=0; i<longVector.length; i++) {
+      var searchedValue = 2**(longVector[i]-1);
+      // Scan rows for the vector
+      for (var r = r0; r < (r0 + 3); r++) {
+        if ( containsDigit(squareValues[c0 + (r * 9)], searchedValue) && 
+            containsDigit(squareValues[c0+1 + (r * 9)], searchedValue) && 
+          containsDigit(squareValues[c0+2 + (r * 9)], searchedValue) ) {
+            Logger.info('Found a 3-digit vector with digit ' + longVector[i] + ' on row ' + r);
+            SpreadsheetApp.getActiveSpreadsheet().getActiveSheet().getRange(COLUMNS.charAt(c0).toString()+(r+1)).setBackground(BG_COLORS[longVector[i]-1]);
+            SpreadsheetApp.getActiveSpreadsheet().getActiveSheet().getRange(COLUMNS.charAt(c0+1).toString()+(r+1)).setBackground(BG_COLORS[longVector[i]-1]);
+            SpreadsheetApp.getActiveSpreadsheet().getActiveSheet().getRange(COLUMNS.charAt(c0+2).toString()+(r+1)).setBackground(BG_COLORS[longVector[i]-1]);
+            changeCount += reduceRow(r, searchedValue, c0, (c0+2));
+          }
+      }
+      // Scan columns for the vector
+      for (var c = c0; c < (c0 + 3); c++) {
+        if ( containsDigit(squareValues[c + (r0 * 9)], searchedValue) && 
+            containsDigit(squareValues[c + ((r0+1) * 9)], searchedValue) && 
+          containsDigit(squareValues[c + ((r0+2) * 9)], searchedValue) ) {
+            Logger.info('Found a 3-digit vector with digit ' + longVector[i] + ' on column ' + c);
+            SpreadsheetApp.getActiveSpreadsheet().getActiveSheet().getRange(COLUMNS.charAt(c).toString()+(r0+1)).setBackground(BG_COLORS[longVector[i]-1]);
+            SpreadsheetApp.getActiveSpreadsheet().getActiveSheet().getRange(COLUMNS.charAt(c).toString()+(r0+1+1)).setBackground(BG_COLORS[longVector[i]-1]);
+            SpreadsheetApp.getActiveSpreadsheet().getActiveSheet().getRange(COLUMNS.charAt(c).toString()+(r0+2+1)).setBackground(BG_COLORS[longVector[i]-1]);
+            changeCount += reduceColumn(c, searchedValue, r0, (r0+2));
+          }
+      }
+    }
+  }
+  
+  return changeCount;
+}
+
+
+function showSmallVectors() {
+  var range = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet().getActiveRange();
+  sheetValues = range.getValues();
+  var changeCount = 0;
+  
+  // Encode values
+  for (var r = 0; r < 9; r++) {
+    for (var c = 0; c < 9; c++) {
+      if (sheetValues[r][c] != 0) {
+        values[c + (r * 9)] = encodeCell(sheetValues[r][c]);
+      }
+    }
+  }
+
+  for (var row = 0; row < 3; row++) {
+    changeCount += findVectorsInSquare(values, 0, (row * 3), true, false);
+    changeCount += findVectorsInSquare(values, 3, (row * 3), true, false);
+    changeCount += findVectorsInSquare(values, 6, (row * 3), true, false);
+  }
+}
+
+function showLongVectors() {
+  var range = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet().getActiveRange();
+  sheetValues = range.getValues();
+  var changeCount = 0;
+  
+  // Encode values
+  for (var r = 0; r < 9; r++) {
+    for (var c = 0; c < 9; c++) {
+      if (sheetValues[r][c] != 0) {
+        values[c + (r * 9)] = encodeCell(sheetValues[r][c]);
+      }
+    }
+  }
+
+  for (var row = 0; row < 3; row++) {
+    changeCount += findVectorsInSquare(values, 0, (row * 3), false, true);
+    changeCount += findVectorsInSquare(values, 3, (row * 3), false, true);
+    changeCount += findVectorsInSquare(values, 6, (row * 3), false, true);
+  }
+}
+
+
+function findVectors() {
+  var changeCount = 0;
+  for (var row = 0; row < 3; row++) {
+    changeCount += findVectorsInSquare(values, 0, (row * 3), true, true);
+    changeCount += findVectorsInSquare(values, 3, (row * 3), true, true);
+    changeCount += findVectorsInSquare(values, 6, (row * 3), true, true);
+  }
+  return changeCount;
+}
